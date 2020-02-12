190eba0a146d09f93db44d52cb650aefdcd4849e
taka1127 <taka07251127@gmail.com>
2020-02-12 18:18:41 +0900

    lesson1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,59 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// -------- 2-1 - 2-2 ----------------------------------------------------
+// Route::get('hello', function () {
+//     return '<html><body><h1>Hello</h1><p>This is sample page.
+//         </p></boby></html>';
+// });
+// -----------------------------------------------------------------------
+
+
+// ---------- 2-3 --------------------------------------------------------
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body { font-size:20pt; color:#999; }
+// h1 { font-size:120pt; text-align:right; color:#eee; margin:40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//     <h1>Hello</h1>
+//     <p>This is sample page!!</p>
+//     <p>これは、サンプルで作ったページです。</p>
+// </body>
+// </html>
+// EOF;
+
+// // Route::get('hello/', function() use ($html) {
+// //     return $html;
+// // });
+// --------------------------------------------------------------------------
+
+// ------------ 2-4 --- 2-5 -------------------------------------------------
+// Route::get('hello/{msg?}', function ($msg='no message.') {
+// $html = <<< EOF
+// <html>
+// <head>
+// <title>こんにちは！</title>
+// <style>
+// body {font-size:16pt; color:#999; }
+// h1 {font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//     <h1>こんにちは！</h1>
+//     <p>{$msg}</p>
+//     <p>これは、サンプルで作ったページです。</p>
+// </body>
+// </html>
+// EOF;
+
+//     return $html;
+// });
+// ---------------------------------------------------------------
+
+Route::get('hello', 'HelloController@index');
