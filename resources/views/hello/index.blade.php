574ec17f7a1427f11507fa7591a6451c90d9b425
taka1127 <taka07251127@gmail.com>
2020-02-20 14:36:26 +0900

    lesson4

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -2,22 +2,69 @@
 <head>
     <title>Hello/Index</title>
     <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:50pt; text-align:center; color:#f6f6f6;
+    body {font-size:16pt; color:black; }
+    h1 { font-size:50pt; text-align:center; color:blue;
           margin:20px 0px -30px 0px; letter-spacing-4pt; }
     </style>
 </head>
+<!-- ---------------foreach文 ------------------------>
+<!-- <body>     
+  <h1>Blade/Index</h1>
+  <p>&#064;foreachディレクティブの例</p>
+  <ol>
+  @foreach($data as $item)
+  <li>{{$item}}</li>
+  @endforeach
+  </ol>
+</body> -->
+<!-- ---------------for文 ------------------------>
+<!-- <body>
+  <h1>Blade/Index</h1>
+  <p>&#064;forディレクティブの例</p>
+  <ol>
+  @for($i = 1; $i < 100; $i++)
+  @if ($i%2==1) 2で割って1余る（奇数の表示）
+      @continue  （奇数を飛ばす）
+  @elseif ($i <= 10)
+  <li>NO,{{$i}}</li>
+  @else
+      @break
+  @endif
+  @endfor
+  </ol>
+</body> -->
+<!-- ---------------$loopによるループ変数 ------------------------>
+<!-- <body>
+  <h1>Blade/Index</h1>
+  <p>&#064;forディレクティブの例</p>
+  @foreach($data as $item)
+  @if ($loop->first)
+  <p>*データ一覧</p>
+  <ul>
+  @endif
+  <li>NO,{{$loop->iteration}}.{{$item}}</li>
+  @if($loop->last)
+  </ul>
+  <p>--ここまで</p>
+  @endif
+  @endforeach
+</body>
+</html> -->
+
+<!-- ---------------phpディレクティブとwhileディレクティブ ------------------------>
 <body>
   <h1>Blade/Index</h1>
-  @if ($msg !='')
-  <p>こんにちは、{{$msg}}さん！</p>
-  @else
-  <p>何か書いてください。</p>
-  @endif
-  <form method="POST" action="/hello" >
-      @csrf
-      <input type="text" name='msg'>
-      <input type="submit">
-  </form>
+  <p>&#064;whileディレクティブの例</p>
+  <ol>
+  @php
+  $counter = 0;
+  @endphp
+  @while($counter < count($data))
+  <li>{{$data[$counter]}}</li>
+  @php
+  $counter++;
+  @endphp
+  @endwhile
+  </ol>
 </body>
 </html>
